257b6d5fa6020f3bcf53ebcafe65476a482cdf74
sleepyqt <sleepyqt@protonmail.com>
2019-03-27 19:05:39 +0300

    try to load dll and setup codegen

--- compiler/llvm_dll.nim
+++ compiler/llvm_dll.nim
@@ -1,6 +1,139 @@
+import std / dynlib
+
+when defined(linux):
+  const llvm_dll = "libLLVM-7.0.0.so"
+  {.pragma: dll, cdecl.}
+
+when defined(windows):
+  const llvm_dll = "LLVM-7.0.dll"
+  {.pragma: dll, stdcall.}
+
+# ------------------------------------------------------------------------------
+
 type
+  Bool* = cint
   ContextRef* = ptr object
   ModuleRef* = ptr object
   TypeRef* = ptr object
   ValueRef* = ptr object
+  BasicBlockRef* = ptr object
+  BuilderRef* = ptr object
+  PassManagerRef* = ptr object
+  PassRegistryRef* = ptr object
 
+type
+  CodeGenOptLevel* {.size: sizeof(cint).} = enum
+    CodeGenLevelNone, CodeGenLevelLess, CodeGenLevelDefault, CodeGenLevelAggressive
+
+  RelocMode* {.size: sizeof(cint).} = enum
+    RelocDefault, RelocStatic, RelocPIC, RelocDynamicNoPic
+
+  CodeModel* {.size: sizeof(cint).} = enum
+    CodeModelDefault, CodeModelJITDefault, CodeModelSmall, CodeModelKernel,
+    CodeModelMedium, CodeModelLarge
+
+  CodeGenFileType* {.size: sizeof(cint).} = enum
+    AssemblyFile, ObjectFile
+
+  TargetMachineRef* = ptr object
+  TargetRef* = ptr object
+
+# ------------------------------------------------------------------------------
+
+var contextCreate*: proc (): ContextRef {.dll.}
+var createBuilderInContext*: proc (c: ContextRef): BuilderRef {.dll.}
+var moduleCreateWithName*: proc (moduleID: cstring): ModuleRef {.dll.}
+
+var int1TypeInContext*: proc(c: ContextRef): TypeRef {.dll.}
+var int8TypeInContext*: proc(c: ContextRef): TypeRef {.dll.}
+var int16TypeInContext*: proc(c: ContextRef): TypeRef {.dll.}
+var int32TypeInContext*: proc(c: ContextRef): TypeRef {.dll.}
+var int64TypeInContext*: proc(c: ContextRef): TypeRef {.dll.}
+var int128TypeInContext*: proc(c: ContextRef): TypeRef {.dll.}
+var intTypeInContext*: proc(c: ContextRef; numBits: cuint): TypeRef {.dll.}
+var floatTypeInContext*: proc(c: ContextRef): TypeRef {.dll.}
+var doubleTypeInContext*: proc(c: ContextRef): TypeRef {.dll.}
+var voidTypeInContext*: proc(c: ContextRef): TypeRef {.dll.}
+
+var getTargetFromTriple*: proc(triple: cstring; t: ptr TargetRef; errorMessage: ptr cstring): Bool {.dll.}
+var disposeMessage*: proc(message: cstring) {.dll.}
+var createTargetMachine*: proc(t: TargetRef; triple: cstring; cpu: cstring; features: cstring; level: CodeGenOptLevel; reloc: RelocMode; codeModel: CodeModel): TargetMachineRef {.dll.}
+var initializeCore*: proc(r: PassRegistryRef) {.dll.}
+var shutdown*: proc() {.dll.}
+var initializeX86Target*: proc() {.dll.}
+var initializeX86TargetInfo*: proc() {.dll.}
+var initializeX86TargetTargetMC*: proc() {.dll.}
+var initializeX86AsmPrinter*: proc() {.dll.}
+var initializeX86AsmParser*: proc() {.dll.}
+var initializeX86Disassembler*: proc() {.dll.}
+var getGlobalPassRegistry*: proc(): PassRegistryRef {.dll.}
+var initializeTransformUtils*: proc(r: PassRegistryRef) {.dll.}
+var initializeScalarOpts*: proc(r: PassRegistryRef) {.dll.}
+var initializeObjCARCOpts*: proc(r: PassRegistryRef) {.dll.}
+var initializeVectorization*: proc(r: PassRegistryRef) {.dll.}
+var initializeInstCombine*: proc(r: PassRegistryRef) {.dll.}
+var initializeIPO*: proc(r: PassRegistryRef) {.dll.}
+var initializeInstrumentation*: proc(r: PassRegistryRef) {.dll.}
+var initializeAnalysis*: proc(r: PassRegistryRef) {.dll.}
+var initializeIPA*: proc(r: PassRegistryRef) {.dll.}
+var initializeCodeGen*: proc(r: PassRegistryRef) {.dll.}
+var initializeTarget*: proc(r: PassRegistryRef) {.dll.}
+var printModuleToFile*: proc(m: ModuleRef; filename: cstring; errorMessage: ptr cstring): Bool {.dll.}
+var writeBitcodeToFile*: proc(m: ModuleRef; path: cstring): cint {.dll.}
+var targetMachineEmitToFile*: proc(t: TargetMachineRef; m: ModuleRef; filename: cstring; codegen: CodeGenFileType; errorMessage: ptr cstring): Bool {.dll.}
+var addFunction*: proc(m: ModuleRef; name: cstring; functionTy: TypeRef): ValueRef {.dll.}
+var functionType*: proc(returnType: TypeRef; paramTypes: ptr TypeRef; paramCount: cuint; isVarArg: Bool): TypeRef {.dll.}
+
+# ------------------------------------------------------------------------------
+
+template get_proc(lib: typed; fun: pointer; name: typed): typed =
+  fun = cast[type(fun)](symAddr(lib, name))
+  if fun == nil: echo "load fail: ", name
+
+proc ll_load_dll*: bool =
+  let lib = loadLib(llvm_dll)
+  result = lib != nil
+  if result:
+    get_proc(lib, contextCreate, "LLVMContextCreate")
+    get_proc(lib, createBuilderInContext, "LLVMCreateBuilderInContext")
+    get_proc(lib, moduleCreateWithName, "LLVMModuleCreateWithName")
+
+    get_proc(lib, int1TypeInContext, "LLVMInt1TypeInContext")
+    get_proc(lib, int8TypeInContext, "LLVMInt8TypeInContext")
+    get_proc(lib, int16TypeInContext, "LLVMInt16TypeInContext")
+    get_proc(lib, int32TypeInContext, "LLVMInt32TypeInContext")
+    get_proc(lib, int64TypeInContext, "LLVMInt64TypeInContext")
+    get_proc(lib, int128TypeInContext, "LLVMInt128TypeInContext")
+    get_proc(lib, intTypeInContext, "LLVMIntTypeInContext")
+    get_proc(lib, floatTypeInContext, "LLVMFloatTypeInContext")
+    get_proc(lib, doubleTypeInContext, "LLVMDoubleTypeInContext")
+    get_proc(lib, voidTypeInContext, "LLVMVoidTypeInContext")
+
+    get_proc(lib, getTargetFromTriple, "LLVMGetTargetFromTriple")
+    get_proc(lib, disposeMessage, "LLVMDisposeMessage")
+    get_proc(lib, createTargetMachine, "LLVMCreateTargetMachine")
+    get_proc(lib, initializeCore, "LLVMInitializeCore")
+    get_proc(lib, shutdown, "LLVMShutdown")
+    get_proc(lib, initializeX86Target, "LLVMInitializeX86Target")
+    get_proc(lib, initializeX86TargetInfo, "LLVMInitializeX86TargetInfo")
+    get_proc(lib, initializeX86TargetTargetMC, "LLVMInitializeX86TargetMC")
+    get_proc(lib, initializeX86AsmPrinter, "LLVMInitializeX86AsmPrinter")
+    get_proc(lib, initializeX86AsmParser, "LLVMInitializeX86AsmParser")
+    get_proc(lib, initializeX86Disassembler, "LLVMInitializeX86Disassembler")
+    get_proc(lib, getGlobalPassRegistry, "LLVMGetGlobalPassRegistry")
+    get_proc(lib, initializeTransformUtils, "LLVMInitializeTransformUtils")
+    get_proc(lib, initializeScalarOpts, "LLVMInitializeScalarOpts")
+    get_proc(lib, initializeObjCARCOpts, "LLVMInitializeObjCARCOpts")
+    get_proc(lib, initializeVectorization, "LLVMInitializeVectorization")
+    get_proc(lib, initializeInstCombine, "LLVMInitializeInstCombine")
+    get_proc(lib, initializeIPO, "LLVMInitializeIPO")
+    get_proc(lib, initializeInstrumentation, "LLVMInitializeInstrumentation")
+    get_proc(lib, initializeAnalysis, "LLVMInitializeAnalysis")
+    get_proc(lib, initializeIPA, "LLVMInitializeIPA")
+    get_proc(lib, initializeCodeGen, "LLVMInitializeCodeGen")
+    get_proc(lib, initializeTarget, "LLVMInitializeTarget")
+    get_proc(lib, printModuleToFile, "LLVMPrintModuleToFile")
+    get_proc(lib, writeBitcodeToFile, "LLVMWriteBitcodeToFile")
+    get_proc(lib, targetMachineEmitToFile, "LLVMTargetMachineEmitToFile")
+    get_proc(lib, addFunction, "LLVMAddFunction")
+    get_proc(lib, functionType, "LLVMFunctionType")
--- compiler/llvm_pass.nim
+++ compiler/llvm_pass.nim
@@ -3,8 +3,10 @@ from options import ConfigRef
 from passes import makePass, skipCodegen
 from transf import transformStmt
 from modulegraphs import PPassContext, ModuleGraph
+from platform import TSystemCPU
 
 import llvm_data, llvm_stmt
+import llvm_dll as llvm
 
 proc myOpen(graph: ModuleGraph; module_sym: PSym): PPassContext =
   echo "myOpen ", module_sym.name.s
@@ -24,13 +26,42 @@ proc myProcess(pass: PPassContext, node: PNode): PNode =
       let new_node = transformStmt(module.module_list.graph, module.module_sym, node)
       gen_stmt_list(module, new_node)
 
-proc llWriteModules*(backend: RootRef, config: ConfigRef) =
-  echo "llWriteModules"
-  let mod_list = BModuleList(backend)
-  for module in mod_list.modules:
-    echo "-- write module --------------------------------"
-    echo module.training_wheels
-    echo "------------------------------------------------"
+# Codegen Setup ----------------------------------------------------------------
 
+proc init_codegen(config: ConfigRef) =
+  case config.target.targetCPU:
+  of cpuI386, cpuAMD64:
+    llvm.initializeX86Target()
+    llvm.initializeX86TargetInfo()
+    llvm.initializeX86TargetTargetMC()
+    llvm.initializeX86AsmParser()
+    llvm.initializeX86AsmPrinter()
+    llvm.initializeX86Disassembler()
+
+    let pass_reg = llvm.getGlobalPassRegistry()
+    llvm.initializeCore(pass_reg)
+    llvm.initializeTransformUtils(pass_reg)
+    llvm.initializeInstrumentation(pass_reg)
+    llvm.initializeCodeGen(pass_reg)
+    llvm.initializeTarget(pass_reg)
+  else: assert(false, "unsupported cpu")
+
+proc shutdown_codegen =
+  llvm.shutdown()
+
+# ------------------------------------------------------------------------------
+
+proc llWriteModules*(backend: RootRef, config: ConfigRef) =
+  if ll_load_dll():
+    init_codegen(config)
+    echo "llWriteModules"
+    let mod_list = BModuleList(backend)
+    for module in mod_list.modules:
+      echo "-- write module --------------------------------"
+      echo module.training_wheels
+      echo "------------------------------------------------"
+    shutdown_codegen()
+  else:
+    echo "cannot load llvm dll :("
 
 const llPass* = makePass(myOpen, myProcess, myClose)
